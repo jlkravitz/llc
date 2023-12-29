b015bc651c1d4a4b79b21c310297bcf684cb88a3
Joshua Kravitz <jlkravitz@gmail.com>
2023-12-29 16:58:23 -0500

    Add CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://jlkravitz.github.io",
-  base: "/llc",
+  site: "https://magicclay.io",
   integrations: [tailwind()],
 });
